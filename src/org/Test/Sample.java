6c008593f5f7fd849ccfb60df1b06d3417f0cbd4
Ramkumar157 <rramkumarishu@gmail.com>
2022-01-21 11:15:45 +0530

    taskdone

---
+++ src/org/Test/Sample.java
@@ -0,0 +1,16 @@
+package org.Test;
+
+public class Sample {
+
+	private void empid() {
+		
+		System.out.println("empid is 1234");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.empid();
+		
+	}
+	
+	
+}
